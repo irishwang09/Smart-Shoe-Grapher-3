6d15d7e9ac2a6cb9006849201180ad997dfcdaaa
Matthew <mmellor@mit.edu>
2016-10-14 15:30:13 -0400

    Real time graphing responsive to 500,000 points x6

--- app/src/main/java/Fragments/GraphFragment.java
+++ app/src/main/java/Fragments/GraphFragment.java
@@ -51,7 +51,7 @@ public class GraphFragment extends Fragment {
     private Handler handler;
 
     private boolean listenerExists = false;
-    private int xBound = 500_000; //
+    private int xBound = 1_000_000; //
     private int yBound = 5000;
     private boolean applyBeenPressed = false;
 
