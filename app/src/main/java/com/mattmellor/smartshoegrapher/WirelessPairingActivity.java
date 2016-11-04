01dc5e6a4bacdf301cb219bd868471307b09480f
Matthew <mmellor@mit.edu>
2016-11-04 16:03:54 -0400

    Implemented method for adding UDPSettingsToDataBase

--- app/src/main/java/com/mattmellor/smartshoegrapher/WirelessPairingActivity.java
+++ app/src/main/java/com/mattmellor/smartshoegrapher/WirelessPairingActivity.java
@@ -1,11 +1,14 @@
 package com.mattmellor.smartshoegrapher;
 
 import android.app.Activity;
+
+import android.content.ContentValues;
 import android.content.pm.ActivityInfo;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import UserDataDataBase.UDPDataBaseHelper;
+import UserDataDataBase.UDPDatabaseContract;
 
 /**
  * Created by Matthew on 10/20/2016.
@@ -14,6 +17,11 @@ import UserDataDataBase.UDPDataBaseHelper;
  */
 
 public class WirelessPairingActivity extends Activity {
+
+    //Fields
+    UDPDataBaseHelper mDbHelper = new UDPDataBaseHelper(getApplicationContext());
+    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +32,20 @@ public class WirelessPairingActivity extends Activity {
 
     //TODO: Allow for dynamic creation of UDP Connection Fragments
 
-    //Open or Create the UDPSensor SQL database
-    UDPDataBaseHelper mDbHelper = new UDPDataBaseHelper(getApplicationContext());
-    SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
+
+
+    public void addUDPSettingsToDataBase(String IPAddress, Integer localPort, Integer remotePort){
+        //Adding a row to the database
+        ContentValues row_value = new ContentValues();
+        row_value.put(UDPDatabaseContract.UdpDataEntry.COLUMN_NAME_IP_HOST, IPAddress);
+        row_value.put(UDPDatabaseContract.UdpDataEntry.COLUMN_NAME_LOCAL_PORT, localPort);
+        row_value.put(UDPDatabaseContract.UdpDataEntry.COLUMN_NAME_REMOTE_PORT, remotePort);
+        long newRowId = db.insert(UDPDatabaseContract.UdpDataEntry.TABLE_NAME, null, row_value);
+    }
+
+    public void readUDPSettingsFromDataBase(){
+        throw new RuntimeException("Unimplemented");
+    }
 
 }
