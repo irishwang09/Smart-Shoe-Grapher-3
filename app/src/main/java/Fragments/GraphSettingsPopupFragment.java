3b67af6658fa02ba7ac2cbdbeaab90de8d75d643
Haripriya Mehta <haripriyamehta@dhcp-18-189-78-139.dyn.mit.edu>
2017-04-03 22:20:24 -0400

    UI for GraphSettings Popup and made an empty fragment file

---
+++ app/src/main/java/Fragments/GraphSettingsPopupFragment.java
@@ -0,0 +1,12 @@
+package Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by haripriyamehta on 4/3/17.
+ */
+
+public class GraphSettingsPopupFragment extends Fragment {
+/*Right now blank
+}
+
