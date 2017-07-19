e5d9bf76cf2ce922072391d62833bce5107de389
hwangir-local <hwangir@mit.edu>
2017-07-19 11:18:47 -0400

    Initial Commit

--- app/src/main/java/Fragments/GraphFragment.java
+++ app/src/main/java/Fragments/GraphFragment.java
@@ -49,7 +49,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.function.DoubleToLongFunction;
 
 import SciChartUserClasses.SciChartBuilder;
 
